daabfd1a1d6fe1c04e374e07f0d770b60a4a67a9
Uditnarayan Yadav <uditnarayan.yadav@atos.net>
2023-04-24 15:23:10 +0530

    first

---
+++ src/test/java/com/assignment/exercise2/Exercise2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.exercise2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exercise2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
